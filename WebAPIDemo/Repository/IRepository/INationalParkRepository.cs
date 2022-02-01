feed475aa0e2d7ec53b450133a43373e54c97826
Upkar Chaudhary <hexaview9@whiteoakuk.com>
2022-02-01 22:48:23 +0530

    Add Repository

---
+++ WebAPIDemo/Repository/IRepository/INationalParkRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Repository.IRepository
+{
+    public interface INationalParkRepository
+    {
+        bool CreateNationalPark(NationalPark natioanlPark);
+        bool DeleteNationalPark(NationalPark nationalPark);
+        bool UpdateNationalPark(NationalPark nationalPark);
+        ICollection<NationalPark> GetNationalParks();
+        NationalPark GetNationalPark();
+        bool IsNationalParkExist(string name);
+        bool IsNationalParkExist(int id);
+        bool save();
+
+    }
+}
---
+++ WebAPIDemo/Repository/IRepository/NationalParkRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIDemo.Models;
+
+namespace WebAPIDemo.Repository.IRepository
+{
+    public class NationalParkRepository : INationalParkRepository
+    {
+        public bool CreateNationalPark(NationalPark natioanlPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+
+        public NationalPark GetNationalPark()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICollection<NationalPark> GetNationalParks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsNationalParkExist(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsNationalParkExist(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateNationalPark(NationalPark nationalPark)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

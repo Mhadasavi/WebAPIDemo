24cf89b491eff26671967c7781fb41f6564bc932
Upkar Chaudhary <hexaview9@whiteoakuk.com>
2022-02-02 12:05:00 +0530

    Add NationalParkController

---
+++ WebAPIDemo/Controllers/NationalParkController.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIDemo.Repository.IRepository;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NationalParkController : Controller
+    {
+        private readonly IMapper _mapper;
+        private readonly INationalParkRepository _nationalParkRepository;
+
+        public NationalParkController(IMapper mapper, INationalParkRepository nationalParkRepository)
+        {
+            _mapper = mapper;
+            _nationalParkRepository = nationalParkRepository;
+        }
+    }
+}

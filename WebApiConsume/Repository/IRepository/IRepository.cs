74099560afca3d9e01a14a29ab420cf3a9755a6f
Upkar Chaudhary <hexaview9@whiteoakuk.com>
2022-03-07 14:53:46 +0530

    Add Repository

---
+++ WebApiConsume/Repository/IRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiConsume.Repository.IRepository
+{
+    interface IRepository<T> where T : class
+    {
+        Task<T> GetAsyn(string URL, int id);
+        Task<IEnumerable<T>> GetAllAsync(string URL);
+        Task<bool> CreateAsync(string URL, T obj);
+        Task<bool> UpdateAsync(string URL, T obj);
+        Task<bool> DeleteAsync(string URL, int id);
+    }
+}

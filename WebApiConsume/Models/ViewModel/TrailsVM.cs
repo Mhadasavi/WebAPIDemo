b86d898be9138880ca87fe34b2f53553b3f8c3f1
Upkar Chaudhary <hexaview9@whiteoakuk.com>
2022-04-02 12:58:02 +0530

    Add Trails Controller

---
+++ WebApiConsume/Models/ViewModel/TrailsVM.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiConsume.Models.ViewModel
+{
+    public class TrailsVM
+    {
+        public IEnumerable<SelectListItem> NationalParks { get; set; }
+        public Trails Trail { get; set; }
+    }
+}

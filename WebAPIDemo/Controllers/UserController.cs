f740a67cdd9203c104655736cc917663351310cd
Upkar <Upkar@DESKTOP-22L0J3T>
2022-04-16 13:37:47 +0530

    Add authenticate method

---
+++ WebAPIDemo/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIDemo.Models;
+using WebAPIDemo.Repository.IRepository;
+
+namespace WebAPIDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        public UserController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] User user)
+        {
+            var userModel = _userRepository.Authenticate(user.Username, user.Password);
+            if (user == null)
+            {
+                return BadRequest(new { message = "Wrong Username or Password" });
+            }
+            return Ok(userModel);
+        }
+    }
+}
